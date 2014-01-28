6fd4cd4d7c63641d8c03f6f3788ecb5e811da241
Sourabh Kulkarni <sourabhk@usc.edu>
2014-01-27 16:44:17 -0800

    Addition

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int add(a,b);
+
+void main()
+{
+	int a=0,b=0;
+
+	printf("Addition is :: %d\n",add(a,b));
+
+}
+
+int add(int a,int b)
+{
+
+	return(a+b);
+
+}
