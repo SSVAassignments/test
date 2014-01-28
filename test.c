5979e667541e06ae812f3a541902a779025e07fe
Sourabh Kulkarni <sourabhk@usc.edu>
2014-01-27 17:23:31 -0800

    Added for the 2nd time

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ int add(a,b);
 void main()
 {
 	int a=0,b=0;
-
+	printf("I've added this..!\n");
 	printf("Addition is :: %d\n",add(a,b));
 
 }
@@ -22,4 +22,10 @@ int add(int a,int b)
 int mult(int a, int b)
 {
 	return(a*b);
+}
+
+int div (int a,int b)
+{
+
+	return(a/b);
 }
